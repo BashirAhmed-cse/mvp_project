e276606cd639f86d918799e5a4b63a1509a8ebab
Bashir <bashirahmed.cse@gmail.com>
2025-09-19 02:24:55 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: 'export', // Remove or comment out this line
+  images: {
+    unoptimized: true, // Optional: Keep if you don’t need image optimization
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
